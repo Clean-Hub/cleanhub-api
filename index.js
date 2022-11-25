f400e289c1f1d775363cb44b305a1aee65e39468
phantware <phantmoney2011@gmail.com>
2022-11-25 12:38:56 +0100

    connection to mongoDB established

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import express from 'express'
-const app = express()
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
+const app = express()
+dotenv.config()
 const PORT = process.env.PORT || 8800
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log('Connected to mongoDB.')
+  } catch (error) {
+    throw error
+  }
+}
+mongoose.connection.on('disconnected', () => {
+  console.log('mongoDB disconnected!')
+})
+
+// Middlewares
+app.use(express.json())
 
 app.get('/', (req, res) => {
   return res.status(200).json({ msg: 'You are welcome to CleanHub API' })
 })
 
 app.listen(PORT, () => {
+  connect()
   console.log(`App running at port ${PORT}`)
 })
