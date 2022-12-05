abdbd7b91709d766e21a2d65fca91cc66c140bfd
phantware <phantmoney2011@gmail.com>
2022-12-05 06:53:29 +0100

    login route adjusted

--- routes/auth.js
+++ routes/auth.js
@@ -43,36 +43,36 @@ router.post('/register', async (req, res) => {
   }
 })
 
-// router.post('/login', async (req, res) => {
-//   try {
-//     const schema = Joi.object({
-//       email: Joi.string().min(3).max(30).required().email(),
-//       password: Joi.string().min(6).max(200).required(),
-//     })
-//     const { error } = schema.validate(req.body)
-//     if (error) return res.status(400).send(error.details[0].message)
+router.post('/login', async (req, res) => {
+  try {
+    const schema = Joi.object({
+      email: Joi.string().min(3).max(30).required().email(),
+      password: Joi.string().min(6).max(200).required(),
+    })
+    const { error } = schema.validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
 
-//     let user = await User.findOne({ email: req.body.email })
-//     if (user) return res.status(400).send('Invalid email or password')
+    let user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(400).send('Invalid email or password')
 
-//     const isPasswordCorrect = await bcrypt.compare(
-//       req.body.password,
-//       user.password
-//     )
-//     if (!isPasswordCorrect)
-//       return res.status(400).send('Invalid email or password')
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    )
+    if (!isPasswordCorrect)
+      return res.status(400).send('Invalid email or password')
 
-//     const token = jwt.sign(
-//       { id: user_id, idAdmin: user.isAdmin },
-//       user.process.env.JWT
-//     )
+    const token = jwt.sign(
+      { id: user._id, email: user.email, idAdmin: user.isAdmin },
+      process.env.JWT
+    )
 
-//     const { password, ...info } = user._doc
+    const { password, ...info } = user._doc
 
-//     return res.status(200).send({ info, token })
-//   } catch (error) {
-//     return res.status(500).send(error)
-//   }
-// })
+    return res.status(200).send({ info, token })
+  } catch (error) {
+    return res.status(500).send(error)
+  }
+})
 
 export default router
